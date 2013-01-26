7f9b9a4f54fcfee05ab7e5789857e92cbd65e7e6
xgfone <xgfone@gmail.com>
2013-01-26 14:28:16 +0800

    Modify "src/local.py".
    In some systems, the currect directory is the directory where the
    symbolic link is, not the true program. So it loads to fail to load
    the locale. Therefore, correct the original relative directory to
    the absolute directory.

--- src/local.py
+++ src/local.py
@@ -3,10 +3,13 @@
 
 import gettext
 import locale
+import os
 
 _lang = locale.getdefaultlocale()[0]
+_parent_dir = os.path.dirname(os.path.dirname(__file__))
+_locale_dir = os.path.join(_parent_dir, 'local')
 
-def install_gettext(domain, localedir='../local', languages=[_lang], fallback=True):
+def install_gettext(domain, localedir=_locale_dir, languages=[_lang], fallback=True):
     _local = gettext.translation(domain, localedir, languages, fallback=fallback)
     _local.install()
     
